b9cd3f6cec88a88aa82970e2a5bd47ed256a9a6f
IlyasElMabrouki <ilyaselmabrouki7@gmail.com>
2024-05-04 22:39:40 +0100

    Added customers component and fixed CORS policy

--- Back-End/src/main/java/com/ilyaselmabrouki/digitalBanking/web/BankAccountRestController.java
+++ Back-End/src/main/java/com/ilyaselmabrouki/digitalBanking/web/BankAccountRestController.java
@@ -3,19 +3,16 @@ package com.ilyaselmabrouki.digitalBanking.web;
 import com.ilyaselmabrouki.digitalBanking.dtos.AccountHistoryDTO;
 import com.ilyaselmabrouki.digitalBanking.dtos.AccountOperationDTO;
 import com.ilyaselmabrouki.digitalBanking.dtos.BankAccountDTO;
-import com.ilyaselmabrouki.digitalBanking.entities.BankAccount;
 import com.ilyaselmabrouki.digitalBanking.exceptions.BankAccountNotFoundException;
 import com.ilyaselmabrouki.digitalBanking.services.IBankAccountService;
 import lombok.AllArgsConstructor;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
 @AllArgsConstructor
+@CrossOrigin("*")
 public class BankAccountRestController {
 
     private IBankAccountService bankAccountService;
